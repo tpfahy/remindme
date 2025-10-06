d425a4ce61af658cd6d4d9770a3ae2c1e5b36a4d
thomasfahy <thomaspfahy@gmail.com>
2025-10-06 19:34:55 -0400

    Initial commit of reminders

---
+++ src/main/java/family/remindme/RemindmeApplication.java
@@ -0,0 +1,13 @@
+package family.remindme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemindmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/family/remindme/RemindmeApplicationTests.java
@@ -0,0 +1,13 @@
+package family.remindme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemindmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
