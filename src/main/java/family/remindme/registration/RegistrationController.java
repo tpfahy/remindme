35fe5194ed6290686483d09cd363677fceaa062e
thomasfahy <thomaspfahy@gmail.com>
2025-10-12 17:41:46 -0400

    fixed tests to actually work

--- src/main/java/family/remindme/registration/RegistrationController.java
+++ src/main/java/family/remindme/registration/RegistrationController.java
@@ -1,4 +1,5 @@
-package family.remindme;
+
+package family.remindme.registration;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/test/java/family/remindme/Test_User.java
+++ src/test/java/family/remindme/Test_User.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class Test_User {
 
-    User superUser = new User();
 
     @Test
     void createUser()
@@ -20,36 +19,45 @@ public class Test_User {
     @Test
     void setFirstName()
     {
+        User superUser = new User();
         assertTrue(superUser.setFirstName("John"));
     }
 
     @Test
     void setLastName()
     {
+        User superUser = new User();
         assertTrue(superUser.setLastName("Doe"));
     }
 
     @Test
     void setPhoneNumber()
     {
+        User superUser = new User();
         assertTrue(superUser.setPhoneNumber("8042796161"));
     }
 
     @Test
     void getFirstName()
     {
+        User superUser = new User();
+        superUser.setFirstName("John");
         assertTrue(superUser.getFirstName().equals("John"));
     }
 
     @Test
     void getLastName()
     {
+        User superUser = new User();
+        superUser.setLastName("Doe");
         assertTrue(superUser.getLastName().equals("Doe"));
     }
 
     @Test
     void getPhoneNumber()
     {
+        User superUser = new User();
+        superUser.setPhoneNumber("8042796161");
         assertTrue(superUser.getPhoneNumber().equals("8042796161"));
     }
 }
